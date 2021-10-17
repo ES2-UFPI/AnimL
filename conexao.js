de51223a5df92beaf6161f5bdc25fab05f3b650b
gabrielscn <38795689+gabrielscn@users.noreply.github.com>
2021-10-16 23:39:01 -0300

    conexao_bd

---
+++ conexao.js
@@ -0,0 +1,41 @@
+const {MongoClient} = require('mongodb');
+
+async function main(){
+    const uri = "mongodb+srv://anime:<senha>@cluster0.fxnst.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+    
+    const client = new MongoClient(uri);
+
+    try{
+        await client.connect();
+
+        await listDatabases(client);
+
+    } catch(e){
+        console.error(e);
+    } finally{
+        await client.close();
+    }   
+}
+
+main().catch(console.error);
+
+async function listDatabases(client){
+    const databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases: ");
+    databasesList.databases.forEach(db =>{
+        console.log(`- ${db.name}`);
+    })
+}
+
+
+
+
+/*const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://anime:<password>@cluster0.fxnst.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object 
+  client.close();
+});*/
