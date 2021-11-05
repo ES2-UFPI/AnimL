6f6bd6c938b57c400b33f8d3cfbb22f657750f8b
Fernando <fernandomirandalolzeiro@gmail.com>
2021-11-05 11:31:11 -0300

    Poder ver informações de um anime especifico #15

--- client/src/pages/anime/index.js
+++ client/src/pages/anime/index.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 
 
-function AnimePage() {
+function Anime(props) {
+    const { animeId } = props.match.params;
+
     const [info, setInfo] = useState({});
     useEffect(()=>{
-      fetch('https://kitsu.io/api/edge/anime/1')
+      fetch(`https://kitsu.io/api/edge/anime/${animeId}`)
           .then((response) => response.json())
           .then((response) =>{
             setInfo(response);
@@ -13,13 +15,23 @@ function AnimePage() {
 
     return (
         <div className="anime-info">
-            <div className="Name"> One piece </div>
-            <div className="Sinopse">{info}</div>
-            <div className="Name"></div>
-
-            
+            <div className="Name">
+                {info.data && (
+                    <div key={info.data.id}>
+                        <h1>{info.data.attributes.canonicalTitle}</h1>
+                        <img
+                            src={info.data.attributes.posterImage.small}
+                            alt={info.data.attributes.canonicalTitle}
+                        />
+                        <div className="info">
+                            <h2>{info.data.attributes.description}</h2>
+                            <h3>{info.data.attributes.synopsis}</h3>     
+                        </div>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
 
-export default AnimePage;
+export default Anime;
--- client/src/routes.js
+++ client/src/routes.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 import { BrowserRouter, Switch, Route} from 'react-router-dom';
 
+
 import Home from './pages/home';
 import Releases from './pages/releases';
-import AnimePage from './pages/anime';
+import Anime from './pages/anime';
 
 export default function Routes(){
     return(
@@ -12,7 +13,7 @@ export default function Routes(){
             <Switch>
                 <Route path="/" exact component={Home}></Route>
                 <Route path="/releases" exact component={Releases}></Route>
-                <Route path="/anime/id" exact component={AnimePage}></Route>
+                <Route path="/anime/:animeId" exact component={Anime}></Route>
             </Switch>
         </BrowserRouter>
     )
